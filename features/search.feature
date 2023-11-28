985d42ed23d2b046d6c0dedc329730f5fd0f16d3
Ramzesito <roman_shv@mail.ru>
2023-11-28 07:44:26 +0300

    Cucumber first test added

--- features/search.feature
+++ features/search.feature
@@ -1,5 +1,11 @@
-Feature: Search a course
-    Scenario: Should search by text
-        Given user is on "/navigation" page
-        When user search by "тестировщик"
-        Then user sees the course suggested "Тестировщик ПО"
+# Feature: Search a course
+#     Scenario: Should search by text
+#         Given user is on "/navigation" page
+#         When user search by "тестировщик"
+#         Then user sees the course suggested "Тестировщик ПО"
+
+Feature: Buying tickets tests
+    Scenario: should successfully buy for tomorrow 1st movie (1 ticket)
+        Given user is on place shoosing page for tomorrow "1" movie
+        When user choose one ticket at "5" row, "6" place
+        Then user sees his tickets selected with notice "Вы выбрали билеты:", "5/6"
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -1,12 +1,16 @@
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { Given, When, Then, Before, After, setDefaultTimeout } = require("cucumber");
+const { clickElement, putText, getText } = require("../../lib/commands.js");
+
+setDefaultTimeout(60000);
 
 Before(async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
   const page = await browser.newPage();
+  await page.goto("https://qamid.tmweb.ru/client/index.php");
+  await page.waitForSelector("h1");
   this.browser = browser;
   this.page = page;
 });
@@ -17,18 +21,52 @@ After(async function () {
   }
 });
 
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
-    setTimeout: 20000,
+// Given("user is on {string} page", async function (string) {
+//   return await this.page.goto(`https://netology.ru${string}`, {
+//     setTimeout: 20000,
+//   });
+// });
+
+// When("user search by {string}", async function (string) {
+//   return await putText(this.page, "input", string);
+// });
+
+// Then("user sees the course suggested {string}", async function (string) {
+//   const actual = await getText(this.page, "a[data-name]");
+//   const expected = await string;
+//   expect(actual).contains(expected);
+// });
+
+  Given("user is on place shoosing page for tomorrow {string} movie", async function (string) {
+    // return await this.page.goto(`https://netology.ru${string}`, {
+    //   setTimeout: 20000,
+    // });
+
+    await clickElement(this.page, "body > nav > a:nth-child(2) > span.page-nav__day-number");
+    await clickElement(this.page, `body > main > section:nth-child(${string}) > div:nth-child(2) > ul > li > a`);
+    //return "pending...";
+
+    // const placeSelector = `body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${buyingScheme[0].row}) > span:nth-child(${buyingScheme[0].place})`;
+    // await clickElement(page, placeSelector);
+    // await clickElement(page, "button.acceptin-button");
+      
+    // let actualCaptionText = await getText(page, "h2.ticket__check-title");
+    // await expect(actualCaptionText).toContain("Вы выбрали билеты:");
+    // let actualTicketsChairs = await getText(page, "span.ticket__chairs");
+    // await expect(actualTicketsChairs).toContain(`${buyingScheme[0].row}/${buyingScheme[0].place}`);
+
   });
-});
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
-});
+  When("user choose one ticket at {string} row, {string} place", async function (row, place) {
+      const placeSelector = `body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${place})`;
+      await clickElement(this.page, placeSelector);
+      await clickElement(this.page, "button.acceptin-button");
+  });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
-  const expected = await string;
-  expect(actual).contains(expected);
-});
+  Then("user sees his tickets selected with notice {string}, {string}", async function (notice, position) {
+      let actualCaptionText = await getText(this.page, "h2.ticket__check-title");
+      await expect(actualCaptionText).contains(notice);
+      let actualTicketsChairs = await getText(this.page, "span.ticket__chairs");
+      await expect(actualTicketsChairs).contains(position);
+
+  });
